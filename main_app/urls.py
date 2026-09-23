42bb36d16f7ad6a3f436173c2fd51f18e860fd67
shahidk07 <shahidkhan88892@gmail.com>
2026-09-23 20:42:34 +0530

    Add exercises view and template for exercise selection

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns=[
 path("",views.home,name="home"),
-# path("exercises/", views.exercise_selection, name="exercise_selection"),
+ path("exercises/", views.exercises, name="exercises"),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"home.html")
+
+
+def exercises(request):
+    return render(request, "exercises.html")
